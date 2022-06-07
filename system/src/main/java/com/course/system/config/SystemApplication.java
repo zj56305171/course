c09f30a693250cd87a76bc9e9bcacb049b27ff14
zhangjie <357301361@qq.com>
2022-06-07 23:12:08 +0800

    1.将通用的配置移动到server 注：server配置文件的路径要放在resources/config/application.properties，不能和上层的路径一样放在resources根目录下

--- system/src/main/java/com/course/system/config/SystemApplication.java
+++ system/src/main/java/com/course/system/config/SystemApplication.java
@@ -1,6 +1,5 @@
-package com.course.system;
+package com.course.system.config;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

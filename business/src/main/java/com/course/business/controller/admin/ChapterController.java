d98f842c2a8b379d869de588103e61569a74a4fe
zhang-jie6 <357301361@qq.com>
2022-11-24 17:52:38 +0800

    1.增加新增大章功能，后端代码开发，完成前后端联调，保存成功

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -28,4 +28,11 @@ public class ChapterController {
         chapterService.list(pageDto);
         return pageDto;
     }
+
+    @RequestMapping("/save")
+    public ChapterDto save(@RequestBody ChapterDto chapterDto){
+        LOG.info("ChapterDto: {}", chapterDto);
+        chapterService.save(chapterDto);
+        return chapterDto;
+    }
 }

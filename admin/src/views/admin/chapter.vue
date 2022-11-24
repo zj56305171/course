abfc906784ada3c415e0e77f4e9ba69d24566bd5
zhang-jie6 <357301361@qq.com>
2022-11-24 14:17:26 +0800

    1.增加分页组件pagination.vue 2.大章管理页面使用分页组件，可自定义初始每页5条，最多显示8个按钮

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,85 +1,98 @@
 <template>
-  <table id="simple-table" class="table  table-bordered table-hover">
-        <thead>
-        <tr>
-          <th>ID</th>
-          <th>名称</th>
-          <th>课程ID</th>
-          <th>操作</th>
-        </tr>
-        </thead>
+  <div>
+    <p>
+      <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-refresh"></i>
+        刷新
+      </button>
+    </p>
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
+    <table id="simple-table" class="table  table-bordered table-hover">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>名称</th>
+        <th>课程ID</th>
+        <th>操作</th>
+      </tr>
+      </thead>
 
-        <tbody>
-        <tr v-for="chapter in chapters">
-          <td>{{chapter.id}}</td>
-          <td>{{chapter.name}}</td>
-          <td>{{chapter.courseId}}</td>
-          <td>
-            <div class="hidden-sm hidden-xs btn-group">
-              <button class="btn btn-xs btn-success">
-                <i class="ace-icon fa fa-check bigger-120"></i>
+      <tbody>
+      <tr v-for="chapter in chapters">
+        <td>{{chapter.id}}</td>
+        <td>{{chapter.name}}</td>
+        <td>{{chapter.courseId}}</td>
+        <td>
+          <div class="hidden-sm hidden-xs btn-group">
+            <button class="btn btn-xs btn-success">
+              <i class="ace-icon fa fa-check bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-info">
+              <i class="ace-icon fa fa-pencil bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-danger">
+              <i class="ace-icon fa fa-trash-o bigger-120"></i>
+            </button>
+
+            <button class="btn btn-xs btn-warning">
+              <i class="ace-icon fa fa-flag bigger-120"></i>
+            </button>
+          </div>
+
+          <div class="hidden-md hidden-lg">
+            <div class="inline pos-rel">
+              <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
+                <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
               </button>
 
-              <button class="btn btn-xs btn-info">
-                <i class="ace-icon fa fa-pencil bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-danger">
-                <i class="ace-icon fa fa-trash-o bigger-120"></i>
-              </button>
-
-              <button class="btn btn-xs btn-warning">
-                <i class="ace-icon fa fa-flag bigger-120"></i>
-              </button>
-            </div>
-
-            <div class="hidden-md hidden-lg">
-              <div class="inline pos-rel">
-                <button class="btn btn-minier btn-primary dropdown-toggle" data-toggle="dropdown" data-position="auto">
-                  <i class="ace-icon fa fa-cog icon-only bigger-110"></i>
-                </button>
-
-                <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
-                  <li>
-                    <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
+              <ul class="dropdown-menu dropdown-only-icon dropdown-yellow dropdown-menu-right dropdown-caret dropdown-close">
+                <li>
+                  <a href="#" class="tooltip-info" data-rel="tooltip" title="View">
 																			<span class="blue">
 																				<i class="ace-icon fa fa-search-plus bigger-120"></i>
 																			</span>
-                    </a>
-                  </li>
+                  </a>
+                </li>
 
-                  <li>
-                    <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
+                <li>
+                  <a href="#" class="tooltip-success" data-rel="tooltip" title="Edit">
 																			<span class="green">
 																				<i class="ace-icon fa fa-pencil-square-o bigger-120"></i>
 																			</span>
-                    </a>
-                  </li>
+                  </a>
+                </li>
 
-                  <li>
-                    <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
+                <li>
+                  <a href="#" class="tooltip-error" data-rel="tooltip" title="Delete">
 																			<span class="red">
 																				<i class="ace-icon fa fa-trash-o bigger-120"></i>
 																			</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
+                  </a>
+                </li>
+              </ul>
             </div>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+          </div>
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </div>
+
 </template>
 
 <script>
+import Pagination from "../../components/pagination";
 export default {
+  components: {Pagination},
   name: "chapter",
   mounted() {
     let _this = this;
+    _this.$refs.pagination.size = 5;
     // sidebar激活样式方法一
     // this.$parent.activeSidebar("business-chapter-sidebar");
-    _this.list();
+    _this.list(1);
   },
   data: function (){
     return{
@@ -87,14 +100,15 @@ export default {
     }
   },
   methods: {
-    list(){
+    list(page){
       let _this = this;
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
-        page: 1,
-        size: 3
+        page: page,
+        size: _this.$refs.pagination.size
       }).then((response)=>{
         console.log("查询大章列表结果",response);
         _this.chapters = response.data.list;
+        _this.$refs.pagination.render(page, response.data.total);
       })
     }
   }
--- doc/db/all.sql
+++ doc/db/all.sql
@@ -17,5 +17,19 @@ create table `chapter` (
         primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='大章';
 
-insert into `chapter` (id, course_id, name) VALUES ('00000000','00000000','测试大章一');
-insert into `chapter` (id, course_id, name) VALUES ('00000001','00000000','测试大章二');
+insert into `chapter` (id, course_id, name) VALUES ('00000001','00000000','测试大章01');
+insert into `chapter` (id, course_id, name) VALUES ('00000002','00000000','测试大章02');
+insert into `chapter` (id, course_id, name) VALUES ('00000003','00000000','测试大章03');
+insert into `chapter` (id, course_id, name) VALUES ('00000004','00000000','测试大章04');
+insert into `chapter` (id, course_id, name) VALUES ('00000005','00000000','测试大章05');
+insert into `chapter` (id, course_id, name) VALUES ('00000006','00000000','测试大章06');
+insert into `chapter` (id, course_id, name) VALUES ('00000007','00000000','测试大章07');
+insert into `chapter` (id, course_id, name) VALUES ('00000008','00000000','测试大章08');
+insert into `chapter` (id, course_id, name) VALUES ('00000009','00000000','测试大章09');
+insert into `chapter` (id, course_id, name) VALUES ('00000010','00000000','测试大章10');
+insert into `chapter` (id, course_id, name) VALUES ('00000011','00000000','测试大章11');
+insert into `chapter` (id, course_id, name) VALUES ('00000012','00000000','测试大章12');
+insert into `chapter` (id, course_id, name) VALUES ('00000013','00000000','测试大章13');
+insert into `chapter` (id, course_id, name) VALUES ('00000014','00000000','测试大章14');
+insert into `chapter` (id, course_id, name) VALUES ('00000015','00000000','测试大章15');
+insert into `chapter` (id, course_id, name) VALUES ('00000016','00000000','测试大章16');

6b926ac6a4a8c3d99b0955b7a58902e2ab40bbdd
zhang-jie6 <357301361@qq.com>
2022-12-04 21:46:46 +0800

    1.保存课程时，同时保存课程分类 2.增加课程分类关联表，一门课程可以属于多个分类

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -169,6 +169,7 @@
         COURSE_LEVEL: COURSE_LEVEL,
         COURSE_CHARGE: COURSE_CHARGE,
         COURSE_STATUS: COURSE_STATUS,
+        tree:{},  //组件变量，并置为空对象
       }
     },
     mounted: function() {
@@ -233,6 +234,13 @@
           return;
         }
 
+        let categorys = _this.tree.getCheckedNodes();
+        if(Tool.isEmpty(categorys)){
+          Toast.warning("请选择分类！");
+          return;  // 要加return
+        }
+        console.log(categorys);
+        _this.course.categorys = categorys;
         Loading.show();
         _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/save', _this.course).then((response)=>{
           Loading.hide();
@@ -304,7 +312,7 @@
 
         let zNodes = _this.categorys;
 
-        $.fn.zTree.init($("#tree"), setting, zNodes);
+        _this.tree = $.fn.zTree.init($("#tree"), setting, zNodes);
 
         // 展开所有的节点
         // _this.tree.expandAll(true);
--- server/src/main/java/com/course/server/service/CourseService.java
+++ server/src/main/java/com/course/server/service/CourseService.java
@@ -29,6 +29,9 @@ public class CourseService {
     @Resource
     private MyCourseMapper myCourseMapper;
 
+    @Resource
+    private CourseCategoryService courseCategoryService;
+
     /**
      * 列表查询
      */
@@ -53,6 +56,9 @@ public class CourseService {
         } else {
             this.update(course);
         }
+
+        //批量保存分类
+        courseCategoryService.saveBatch(courseDto.getId(),courseDto.getCategorys());
     }
 
     /**

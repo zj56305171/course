56c2156117e52cef5d2328e3724939b368f414bc
zhang-jie6 <357301361@qq.com>
2022-12-06 22:57:40 +0800

    1.课程管理增加自定义排序

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -53,6 +53,9 @@
               <button v-on:click="editContent(course)" class="btn btn-white btn-xs btn-info btn-round">
                 内容
               </button>&nbsp;
+              <button v-on:click="openSortModal(course)" class="btn btn-white btn-xs btn-info btn-round">
+                排序
+              </button>&nbsp;
               <button v-on:click="edit(course)" class="btn btn-white btn-xs btn-info btn-round">
                 编辑
               </button>&nbsp;
@@ -143,7 +146,7 @@
               <div class="form-group">
                 <label class="col-sm-2 control-label">顺序</label>
                 <div class="col-sm-10">
-                  <input v-model="course.sort" class="form-control">
+                  <input v-model="course.sort" class="form-control" disabled>
                 </div>
               </div>
             </form>
@@ -189,6 +192,36 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
+    <div id="course-sort-modal" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">排序</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <label class="col-lg-3 control-label">当前排序</label>
+                <div class="col-lg-9">
+                  <input v-model="sort.oldSort" class="form-control" name="oldSort" disabled>
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-lg-3 control-label">新排序</label>
+                <div class="col-lg-9">
+                  <input v-model="sort.newSort" class="form-control" name="newSort">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button v-on:click="updateSortModal()" type="button" class="btn btn-primary">保存</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
 </template>
 
@@ -206,6 +239,11 @@
         COURSE_STATUS: COURSE_STATUS,
         tree:{},  //组件变量，并置为空对象
         saveContentLabel: "",
+        sort:{
+          id: "",
+          oldSort: 0,
+          newSort: 0
+        }
       }
     },
     mounted: function() {
@@ -223,7 +261,9 @@
        */
       add() {
         let _this = this;
-        _this.course = {};
+        _this.course = {
+          sort: _this.$refs.pagination.total + 1
+        };
         _this.tree.getCheckedNodes();
         $("#form-modal").modal("show");
       },
@@ -433,6 +473,42 @@
             _this.saveContentLabel = "最后保存时间" + now;
           }else{
             Toast.warning(resp.message);
+          }
+        });
+      },
+
+      /**
+       * 打开排序框
+       */
+      openSortModal(course){
+        let _this = this;
+        _this.sort = {
+          id: course.id,
+          oldSort: course.sort,
+          newSort: course.sort,
+        }
+        $("#course-sort-modal").modal("show");
+      },
+
+      /**
+       * 排序
+       */
+      updateSortModal(){
+        let _this = this;
+        if(_this.sort.oldSort === _this.sort.newSort){
+          Toast.warning("排序没有变化！");
+          return;
+        }
+        Loading.show();
+        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/sort/', _this.sort).then((res)=>{
+          Loading.hide();
+          let response = res.data;
+          if (response.success) {
+            Toast.success("更新排序成功！");
+            $("#course-sort-modal").modal("hide");
+            _this.list(1);
+          } else {
+            Toast.error("更新排序失败！");
           }
         });
       },
--- server/src/main/java/com/course/server/mapper/my/MyCourseMapper.java
+++ server/src/main/java/com/course/server/mapper/my/MyCourseMapper.java
@@ -1,7 +1,14 @@
 package com.course.server.mapper.my;
 
 import org.apache.ibatis.annotations.Param;
+import com.course.server.dto.SortDto;
 
 public interface MyCourseMapper {
     int updateTime(@Param("courseId") String courseId);
+
+    int updateSort(SortDto sortDto);
+
+    int moveSortsBackward(SortDto sortDto);
+
+    int moveSortsForward(SortDto sortDto);
 }

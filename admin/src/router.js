14de1afa965710d0b4a6293eeba2346aa5853e90
zhang-jie6 <357301361@qq.com>
2022-12-07 11:40:26 +0800

    1.增加讲师管理功能

--- admin/src/router.js
+++ admin/src/router.js
@@ -7,6 +7,7 @@ import Course from "./views/admin/course.vue"
 import Chapter from "./views/admin/chapter.vue"
 import Section from "./views/admin/section.vue"
 import Category from "./views/admin/category.vue"
+import Teacher from "./views/admin/teacher.vue"
 
 Vue.use(Router);
 
@@ -42,6 +43,10 @@ export default new Router({
             path: "business/section",
             name: "business/section",
             component: Section,
+        }, {
+            path: "business/teacher",
+            name: "business/teacher",
+            component: Teacher,
         }]
     }]
 })

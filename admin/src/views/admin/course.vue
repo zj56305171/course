4d1a2099df773c5fdc8a7d416c75a052739f018e
zhang-jie6 <357301361@qq.com>
2022-12-28 21:22:52 +0800

    1.文件上传功能开发：将文件内容模态框做成单独的页面，代码更容易维护，内容编辑更方便

--- admin/src/views/admin/course.vue
+++ admin/src/views/admin/course.vue
@@ -60,7 +60,7 @@
               <button v-on:click="toChapter(course)" class="btn btn-white btn-xs btn-info btn-round">
                 大章
               </button>&nbsp;
-              <button v-on:click="editContent(course)" class="btn btn-white btn-xs btn-info btn-round">
+              <button v-on:click="toContent(course)" class="btn btn-white btn-xs btn-info btn-round">
                 内容
               </button>&nbsp;
               <button v-on:click="openSortModal(course)" class="btn btn-white btn-xs btn-info btn-round">
@@ -186,70 +186,6 @@
         </div><!-- /.modal-content -->
       </div><!-- /.modal-dialog -->
     </div><!-- /.modal -->
-    <div id="course-content-modal" class="modal fade" tabindex="-1" role="dialog">
-      <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-            <h4 class="modal-title">内容编辑</h4>
-          </div>
-          <div class="modal-body">
-            <file v-bind:input-id="'content-file-upload'"
-                  v-bind:text="'上传文件'"
-                  v-bind:suffixs="['jpg', 'jpeg', 'png', 'mp4']"
-                  v-bind:use="FILE_USE.COURSE.key"
-                  v-bind:after-upload="afterUploadContentFile"></file>
-            <br>
-            <table id="file-table" class="table  table-bordered table-hover">
-              <thead>
-              <tr>
-                <th>名称</th>
-                <th>地址</th>
-                <th>大小</th>
-                <th>操作</th>
-              </tr>
-              </thead>
-
-              <tbody>
-              <tr v-for="(f, i) in files" v-bind:key="f.id">
-                <td>{{f.name}}</td>
-                <td>{{f.url}}</td>
-                <td>{{f.size | formatFileSize}}</td>
-                <td>
-                  <button v-on:click="delFile(f)" class="btn btn-white btn-xs btn-warning btn-round">
-                    <i class="ace-icon fa fa-times red2"></i>
-                    删除
-                  </button>
-                </td>
-              </tr>
-              </tbody>
-            </table>
-            <form class="form-horizontal">
-              <div class="form-group">
-                <div class="col-lg-12">
-                  {{saveContentLabel}}
-                </div>
-              </div>
-              <div class="form-group">
-                <div class="col-lg-12">
-                  <div id="content"></div>
-                </div>
-              </div>
-            </form>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-white btn-default btn-round" data-dismiss="modal">
-              <i class="ace-icon fa fa-times"></i>
-              取消
-            </button>
-            <button v-on:click="saveContent()" type="button" class="btn btn-white btn-info btn-round">
-              <i class="ace-icon fa fa-plus blue"></i>
-              保存
-            </button>
-          </div>
-        </div><!-- /.modal-content -->
-      </div><!-- /.modal-dialog -->
-    </div><!-- /.modal -->
     <div id="course-sort-modal" class="modal fade" tabindex="-1" role="dialog">
       <div class="modal-dialog" role="document">
         <div class="modal-content">
@@ -305,7 +241,6 @@
           newSort: 0
         },
         teachers: [],
-        files: [],
       }
     },
     mounted: function() {
@@ -415,12 +350,21 @@
       },
 
       /**
-       * 点击【编辑】
+       * 点击【大章】
        */
       toChapter(course) {
         let _this = this;
         SessionStorage.set(SESSION_KEY_COURSE,course);
         _this.$router.push("/business/chapter")
+      },
+
+      /**
+       * 点击【内容】
+       */
+      toContent(course) {
+        let _this = this;
+        SessionStorage.set(SESSION_KEY_COURSE, course);
+        _this.$router.push("/business/content");
       },
 
       allCategory() {
@@ -482,71 +426,6 @@
       },
 
       /**
-       * 打开内容编辑框
-       */
-      editContent(course) {
-        let _this = this;
-        let id = course.id;
-        _this.course = course;
-        $("#content").summernote({
-          focus:true,
-          height:300
-        });
-
-        //清空历史文本
-        $("#content").summernote('code','');
-        _this.saveContentLabel="";
-
-        //加载内容文件列表
-        _this.listContentFiles();
-
-        Loading.show();
-        _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course/find-content/' + id).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if(resp.success){
-            $("#course-content-modal").modal({backdrop:'static', keyboard: false});
-            if(resp.content){
-              $("#content").summernote('code',resp.content.content);
-            }
-            //定时自动保存
-            let saveContentInterval = setInterval(function (){
-              _this.saveContent();
-            }, 5000);
-            // 关闭内容框时，清除自动保存任务
-            $("#course-content-modal").on('hidden.bs.modal',function (e){
-              clearInterval(saveContentInterval);
-            });
-          }else{
-            Toast.warning(resp.message);
-          }
-        });
-      },
-
-      /**
-       * 打开内容编辑框
-       */
-      saveContent(course) {
-        let _this = this;
-        let content = $("#content").summernote('code');
-        Loading.show();
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course/save-content', {
-          id: _this.course.id,
-          content: content
-        }).then((response)=>{
-          Loading.hide();
-          let resp = response.data;
-          if(resp.success){
-            // Toast.success("内容保存成功");
-            let now = Tool.dateFormat("mm:ss");
-            _this.saveContentLabel = "最后保存时间" + now;
-          }else{
-            Toast.warning(resp.message);
-          }
-        });
-      },
-
-      /**
        * 打开排序框
        */
       openSortModal(course){
@@ -596,53 +475,6 @@
         let _this = this;
         let image = resp.content.path;
         _this.course.image = image;
-      },
-
-      /**
-       * 加载内容文件列表
-       */
-      listContentFiles() {
-        let _this = this;
-        _this.$ajax.get(process.env.VUE_APP_SERVER + '/business/admin/course-content-file/list/' + _this.course.id).then((response)=>{
-          let resp = response.data;
-          if (resp.success) {
-            _this.files = resp.content;
-          }
-        });
-      },
-
-      /**
-       * 上传内容文件后，保存内容文件记录
-       */
-      afterUploadContentFile(response) {
-        let _this = this;
-        console.log("开始保存文件记录");
-        let file = response.content;
-        file.courseId = _this.course.id;
-        file.url = file.path;
-        _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/admin/course-content-file/save', file).then((response)=>{
-          let resp = response.data;
-          if (resp.success) {
-            Toast.success("上传文件成功");
-            _this.files.push(resp.content);
-          }
-        });
-      },
-
-      /**
-       * 删除内容文件
-       */
-      delFile(f) {
-        let _this = this;
-        Confirm.show("删除课程后不可恢复，确认删除？", function () {
-          _this.$ajax.delete(process.env.VUE_APP_SERVER + '/business/admin/course-content-file/delete/' + f.id).then((response)=>{
-            let resp = response.data;
-            if (resp.success) {
-              Toast.success("删除文件成功");
-              Tool.removeObj(_this.files, f);
-            }
-          });
-        });
       },
     }
   }
